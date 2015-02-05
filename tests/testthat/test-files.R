ed920a1a6355601faf3a78e30f7b90e08b114252
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2015-02-05 11:50:03 -0500

    Fixed paths in test

--- tests/testthat/test-files.R
+++ tests/testthat/test-files.R
@@ -2,8 +2,8 @@ context('File Coverage')
 
 test_that('File coverage works correctly (simple)', {
    before <- ls()
-   cov <- ReportCoverageFiles("~/rcov/tests//testthat/FileCoverage/files/functions1.R", 
-                              "~/rcov/tests//testthat/FileCoverage/tests//tests1.R")
+   cov <- ReportCoverageFiles("./FileCoverage/files/functions1.R", 
+                              "./FileCoverage/tests//tests1.R")
    expect_equal(c(before, "before", "cov"), ls())
    expect_equal(row.names(cov), c("function1"))
    expect_equal(as.numeric(cov$cov[1]), 1)
@@ -11,27 +11,27 @@ test_that('File coverage works correctly (simple)', {
 
 test_that('File coverage works correctly (multiple tests and functions)', {
     before <- ls()
-    cov <- ReportCoverageFiles("~/rcov/tests//testthat/FileCoverage/files/functions2.R", 
-                               c("~/rcov/tests//testthat/FileCoverage/tests//tests2.R", 
-                                 "~/rcov/tests//testthat/FileCoverage/tests//tests3.R"))
+    cov <- ReportCoverageFiles("./FileCoverage/files/functions2.R", 
+                               c("./FileCoverage/tests//tests2.R", 
+                                 "./FileCoverage/tests//tests3.R"))
     expect_equal(c(before, "before", "cov"), ls())
     expect_equal(row.names(cov), c("function1"))
     expect_equal(cov$mstmt[1], 2)
-    cov <- ReportCoverageFiles("~/rcov/tests//testthat/FileCoverage/files/functions2.R", 
-                               c("~/rcov/tests//testthat/FileCoverage/tests//tests2.R", 
-                                 "~/rcov/tests//testthat/FileCoverage/tests//tests3.R", 
-                                 "~/rcov/tests//testthat/FileCoverage/tests//tests4.R"))
+    cov <- ReportCoverageFiles("./FileCoverage/files/functions2.R", 
+                               c("./FileCoverage/tests//tests2.R", 
+                                 "./FileCoverage/tests//tests3.R", 
+                                 "./FileCoverage/tests//tests4.R"))
     expect_equal(c(before, "before", "cov"), ls())
     expect_equal(row.names(cov), c("function1"))
     expect_equal(cov$mstmt[1], 0)
-    cov <- ReportCoverageFiles("~/rcov/tests//testthat/FileCoverage/files/functions2.R", 
-                               c("~/rcov/tests//testthat/FileCoverage/tests2/"))
+    cov <- ReportCoverageFiles("./FileCoverage/files/functions2.R", 
+                               c("./FileCoverage/tests2/"))
     expect_equal(c(before, "before", "cov"), ls())
     expect_equal(row.names(cov), c("function1"))
     expect_equal(cov$mstmt[1], 0)
-    cov <- ReportCoverageFiles("~/rcov/tests//testthat/FileCoverage/files/functions2.R", 
-                               c("~/rcov/tests//testthat/FileCoverage/tests//tests2.R", 
-                                 "~/rcov/tests//testthat/FileCoverage/tests//tests3.R"), report.by.file = T)
+    cov <- ReportCoverageFiles("./FileCoverage/files/functions2.R", 
+                               c("./FileCoverage/tests//tests2.R", 
+                                 "./FileCoverage/tests//tests3.R"), report.by.file = T)
     expect_equal(c(before, "before", "cov"), ls())
     expect_true(grepl("functions2.R", row.names(cov)[1]))
     expect_equal(cov$mstmt[1], 2)
